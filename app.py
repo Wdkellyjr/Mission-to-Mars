fba26da76b36c22b6ab8617e28a30c50dfaba5ca
Warren D Kelly Jr <wdkellyjr@warrens-mbp-2.myfiosgateway.com>
2021-11-07 01:03:58 -0400

    final add.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import Scraping
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import Scraping
+
 
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
+# Use flask_PyMongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
@@ -16,9 +17,9 @@ def index():
 @app.route("/scrape")
 def scrape():
     mars = mongo.db.mars
-    mars_data = scraping.scrape_all()
-    mars.update({}, mars_data, upsert=True)
+    mars_data = Scraping.scrape_all()
+    mars.update({},mars_data, upsert=True)
     return redirect('/', code=302)
-
+    
 if __name__ == "__main__":
-   app.run()
+    app.run()
